830d2d7becdb7385baef058d93237ef325c4dbae
Ermolenko Elena <leo11_94@mail.ru>
2023-07-03 15:24:35 +0300

    дз

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+        // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+        Console.WriteLine("Введите число 1: ");
+        Console.WriteLine("Введите число 2: ");
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("Первое число большее");
+        }
+        else 
+        {
+            Console.WriteLine("Второе число большее");
+        }
+        if (num1 < num2)
+        {
+            Console.WriteLine("Первое число меньшее");
+        }
+        else 
+        {
+            Console.WriteLine("Второе число меньшее");
+        }
---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число 1: ");
+Console.WriteLine("Введите число 2: ");
+Console.WriteLine("Введите число 3: ");
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+            if (num2 > max)
+            {
+                max = num2;
+            }
+
+            if (num3 > max)
+            {
+                max = num3;
+            }
+ 
+    Console.WriteLine("Наибольшее из введённых чисел -> " + max);
